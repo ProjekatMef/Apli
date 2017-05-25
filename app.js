c9577d7998613efcfd7a56315eba33c0b4755029
Luka Ilic <lyksenzii@gmail.com>
2017-05-25 10:17:04 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,24 +3,48 @@ window.onload = function() {
 	dugmeDodajZadatak.addEventListener("click", dodajZadatak);
 }
 
+function AjaxZahtev(zadaciZaSlanje, callback) {
+	var req = new XMLHttpRequest();
+	req.open(zadaciZaSlanje.metod, zadaciZaSlanje.putanja, true);
+	req.addEventListener("load", function() {
+		if (req.status < 400) {
+			callback(req.responseText);
+		}
+		else {
+			callback(new Error("Request failed: " + req.statusText));
+		}
+	});
+	req.addEventListener("error", function() {
+		callback(new Error("Network error"));
+	});
+	req.send(zadaciZaSlanje.zadatak || null);
+}
+
 function dodajZadatak() {
 	var korisnicko_ime = document.querySelector("#korisnicko-ime").value;
 	var zadatak = document.querySelector("#zadatak").value;
 
-	if(!validnostPodataka(korisnicko_ime, zadatak)) {
+	if(!validnostPodataka(korisnicko_ime)) {
 		alert("Pogresan unos podataka!");
 	}
 
-	var zadaci = {};
-	zadaci.metod = "post";
-	zadaci.putanja = "posalji";
-	zadaci.korisnik = korisnicko_ime;
-	zadaci.zadatak = zadatak;
-	alert(zadaci.korisnik+"\n"+zadaci.zadatak);
+	var zadaciZaSlanje = {};
+	zadaciZaSlanje.metod = "post";
+	zadaciZaSlanje.putanja = "novi-zadatak";
+	var korisnik = {
+		"ime" : korisnicko_ime,
+		"zadatak" : zadatak
+	};
+	zadaciZaSlanje.zadatak = JSON.stringify(korisnik);
+	AjaxZahtev(zadaciZaSlanje, odgovorServeraP);
 }
 
-function validnostPodataka(korisnicko_ime, zadatak) {
-	if(korisnicko_ime == "" || zadatak == "") {
+function odgovorServeraP(odgovor) {
+	console.log(odgovor);
+}
+
+function validnostPodataka(korisnicko_ime) {
+	if(korisnicko_ime == "") {
 		return false;
 	}
 	return true;
--- server.js
+++ server.js
@@ -1,10 +1,81 @@
 var http = require("http");
-var server = http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-type":"text/html"});
-	response.write("<h1>Ja sam naslov.</h1>");
-	response.end();
+var fs = require("fs");
+
+var korisnici = [];
+var zadaci = [];
+
+fs.readFile("korisnici.dat", function read(err, data) {
+	if(err) {
+		korisnici = [{ime : "admin", zadatak : "Sastanak"}];
+	}
+	else {
+		korisnici = JSON.parse(data);
+	}
 });
 
-server.listen(8000);
+fs.readFile("zadaci.dat", function read(err, data) {
+	if(err) {
+		zadaci = [];
+	}
+	else {
+		zadaci = JSON.parse(data);
+	}
+});
 
-console.log("Cekam na portu 8000....")
+var index = fs.readFileSync("index.html", "utf8");
+var style = fs.readFileSync("style.css", "utf8");
+var appjs = fs.readFileSync("app.js", "utf8");
+
+function prikaziInterfejs(response) {
+	response.writeHead(200, {"Content-type" : "text/html"});
+	response.end(index);
+}
+
+function greskaURL(response) {
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<h1>Stranica1 nije pronadjena..");
+	response.end();
+}
+
+function zadatak(zadatak) {
+	zadaci.push(zadatak);
+	fs.writeFile("zadaci.dat", JSON.stringify(zadaci), function(err) {
+		if(err) {
+			console.log(err);
+		}
+	});
+}
+
+function odgovorServera(request, response) {
+	switch(request.url) {
+		case "/":
+		case "/index.html":
+			prikaziInterfejs(response);
+			break;
+		case "/style.css":
+			response.writeHead(200, {"Content-Type": "text/css"});
+			response.end(style);
+			break;
+		case "/app.js":
+			response.writeHead(200, {"Content-Type": "text/pain"});
+			response.end(appjs);
+			break;
+		case "/novi-zadatak":
+			var zadatak = "";
+			request.on("data", function(data) {
+				zadatak += data;
+			});
+			request.on("end", function() {
+				zadatak = JSON.parse(zadatak);
+				response.end(zadatak(zadatak));
+			});
+			break;
+		default:
+			greskaURL(response);
+			break;
+	}
+}
+
+var server = http.createServer(odgovorServera);
+server.listen(8080);
+console.log("Server startovan na portu 8080");
