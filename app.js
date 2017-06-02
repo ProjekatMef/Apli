c10e8a99e190d4a17eb08e4a2df5f0a0d1fbdc1e
Luka Ilic <lyksenzii@gmail.com>
2017-06-02 23:27:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,88 +1,88 @@
 window.onload = function() {
-	var dugmeDodajZadatak = document.querySelector("#posalji");
-	dugmeDodajZadatak.addEventListener("click", dodajZadatak);
+    var dugmeDodajZadatak = document.querySelector("#posalji");
+    dugmeDodajZadatak.addEventListener("click", dodajZadatak);
 }
 
 function AjaxZahtev(options, callback) {
-	var req = new XMLHttpRequest();
-	req.open(options.metod, options.putanja, true);
-	req.addEventListener("load", function() {
-		if (req.status < 400) {
-			callback(req.responseText);
-		}
-		else {
-			callback(new Error("Request failed: " + req.statusText));
-		}
-	});
-	req.addEventListener("error", function() {
-		callback(new Error("Network error"));
-	});
-	req.send(options.zadatak || null);
+    var req = new XMLHttpRequest();
+    req.open(options.metod, options.putanja, true);
+    req.addEventListener("load", function() {
+        if (req.status < 400) {
+            callback(req.responseText);
+        }
+        else {
+            callback(new Error("Request failed: " + req.statusText));
+        }
+    });
+    req.addEventListener("error", function() {
+        callback(new Error("Network error"));
+    });
+    req.send(options.zadatak || null);
 }
 
 function korisnik_() {
-	var korisnicko_ime = document.querySelector("#korisnicko-ime").value;
-	var options = {};
-	options.metod = "post";
-	options.putanja = "korisnik";
-	var korisnik = {
-		"ime" : korisnicko_ime,
-	};
-	options.zadatak = JSON.stringify(korisnik);
-	AjaxZahtev(options, odgovorServeraP);
+    var korisnicko_ime = document.querySelector("#korisnicko-ime").value;
+    var options = {};
+    options.metod = "post";
+    options.putanja = "korisnik";
+    var korisnik = {
+        "ime" : korisnicko_ime,
+    };
+    options.zadatak = JSON.stringify(korisnik);
+    AjaxZahtev(options, odgovorServeraP);
 }
 
 function dodajZadatak() {
-	var prikazi = document.querySelector("#zadatak-prikazi");
-	prikazi.innerHTML = "";
+    var prikazi = document.querySelector("#zadatak-prikazi");
+    prikazi.innerHTML = "";
 
-	var korisnicko_ime = document.querySelector("#korisnicko-ime").value;
-	var zadatak = document.querySelector("#zadatak").value;
+    var korisnicko_ime = document.querySelector("#korisnicko-ime").value;
+    var zadatak = document.querySelector("#zadatak").value;
 
-	if(!validnostPodataka(korisnicko_ime)) {
-		alert("Pogresan unos podataka!");
-	}
+    if(!validnostPodataka(korisnicko_ime)) {
+        alert("Pogresan unos podataka!");
+    }
 
-	var options = {};
-	options.metod = "post";
-	options.putanja = "novi-zadatak";
-	var korisnik = {
-		"ime" : korisnicko_ime,
-		"zadatak" : zadatak
-	};
-	options.zadatak = JSON.stringify(korisnik);
-	//alert(options)
-	AjaxZahtev(options, odgovorServeraP);
+    var options = {};
+    options.metod = "post";
+    options.putanja = "novi-zadatak";
+    var korisnik = {
+        "ime" : korisnicko_ime,
+        "zadatak" : zadatak
+    };
+    options.zadatak = JSON.stringify(korisnik);
+    //alert(options)
+    AjaxZahtev(options, odgovorServeraP);
 
-	//korisnik_()
+    //korisnik_()
 }
 
 function Brisanje() {
-	alert("test")
+    alert("test")
 }
 
 function brisiZadatak(sta) {
-	zadatak = sta.nextElementSibling.innerHTML;
+    zadatak = sta.nextElementSibling.innerHTML;
     alert(zadatak);
 }
 
 function odgovorServeraP(odgovor) {
-	var id = 1;
-	zadaci = JSON.parse(odgovor);
-	var prikazi = document.querySelector("#zadatak-prikazi");
-	for(var i = 0; i<zadaci.length; i++) {
-		//prikazi.innerHTML += "<p><span class='dugme-code' onclick='izbrisi(this)'>&#128465;</span>"+zadaci[i].zadatak+"</p>";
-		//prikazi.innerHTML += "<p id='"+id+"'><a href='javascript:izbrisi("+id+")'>X </a>"+zadaci[i].zadatak+"</p>";
-		//id++;
-		prikazi.innerHTML += "<p><span class='dugme-code' onclick='brisiZadatak(this)'>&#128465;</span><span>"+zadaci[i].zadatak+"</span></p>";
-	}
+    var id = 1;
+    zadaci = JSON.parse(odgovor);
+    var prikazi = document.querySelector("#zadatak-prikazi");
+    for(var i = 0; i<zadaci.length; i++) {
+        //prikazi.innerHTML += "<p><span class='dugme-code' onclick='izbrisi(this)'>&#128465;</span>"+zadaci[i].zadatak+"</p>";
+        //prikazi.innerHTML += "<p id='"+id+"'><a href='javascript:izbrisi("+id+")'>X </a>"+zadaci[i].zadatak+"</p>";
+        //id++;
+        prikazi.innerHTML += "<p><span class='dugme-code' onclick='brisiZadatak(this)'>&#128465;</span><span>"+zadaci[i].zadatak+"</span></p>";
+    }
 }
-	
+    
 
 function validnostPodataka(korisnicko_ime) {
-	if(korisnicko_ime == "") {
-		return false;
-	}
-	return true;
+    if(korisnicko_ime == "") {
+        return false;
+    }
+    return true;
 }
 
--- server.js
+++ server.js
@@ -5,21 +5,21 @@ var korisnici = [];
 var zadaci = [];
 
 fs.readFile("korisnici.dat", function read(err, data) {
-	if(err) {
-		korisnici = [];
-	}
-	else {
-		korisnici = JSON.parse(data);
-	}
+    if(err) {
+        korisnici = [];
+    }
+    else {
+        korisnici = JSON.parse(data);
+    }
 });
 
 fs.readFile("zadaci.dat", function read(err, data) {
-	if(err) {
-		zadaci = [];
-	}
-	else {
-		zadaci = JSON.parse(data);
-	}
+    if(err) {
+        zadaci = [];
+    }
+    else {
+        zadaci = JSON.parse(data);
+    }
 });
 
 var index = fs.readFileSync("index.html", "utf8");
@@ -27,88 +27,88 @@ var style = fs.readFileSync("style.css", "utf8");
 var appjs = fs.readFileSync("app.js", "utf8");
 
 function prikaziInterfejs(response) {
-	response.writeHead(200, {"Content-type" : "text/html"});
-	response.end(index);
+    response.writeHead(200, {"Content-type" : "text/html"});
+    response.end(index);
 }
 
 function greskaURL(response) {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("<h1>Stranica1 nije pronadjena..");
-	response.end();
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>Stranica1 nije pronadjena..");
+    response.end();
 }
 
 function zadatak_(zadatak) {
-	zadaci.push(zadatak);
-	fs.writeFile("zadaci.dat", JSON.stringify(zadaci), function(err) {
-		if(err) {
-			console.log(err);
-		}
-	});
+    zadaci.push(zadatak);
+    fs.writeFile("zadaci.dat", JSON.stringify(zadaci), function(err) {
+        if(err) {
+            console.log(err);
+        }
+    });
 
-	return JSON.stringify(zadaci);
+    return JSON.stringify(zadaci);
 }
 
 function korisnik_(korisnik) {
-	korisnici.push(korisnik);
-	fs.writeFile("korisnici.dat", JSON.stringify(korisnici), function(err) {
-		if(err) {
-			console.log(err);
-		}
-	});
+    korisnici.push(korisnik);
+    fs.writeFile("korisnici.dat", JSON.stringify(korisnici), function(err) {
+        if(err) {
+            console.log(err);
+        }
+    });
 }
 
 function odgovorServera(request, response) {
-	switch(request.url) {
-		case "/":
-		case "/index.html":
-			prikaziInterfejs(response);
-			break;
-		case "/style.css":
-			response.writeHead(200, {"Content-Type": "text/css"});
-			response.end(style);
-			break;
-		case "/app.js":
-			response.writeHead(200, {"Content-Type": "text/pain"});
-			response.end(appjs);
-			break;
-		case "/novi-zadatak":
-			var ime = "";
-			var zadatak = "";
-			request.on("data", function(data) {
-				zadatak += data;
-			});
-			request.on("end", function() {
-				zadatak = JSON.parse(zadatak);
-				//console.log(zadatak);
-				ime = zadatak.ime;
-				zadatak_(zadatak);
-				var odgovor = [];
+    switch(request.url) {
+        case "/":
+        case "/index.html":
+            prikaziInterfejs(response);
+            break;
+        case "/style.css":
+            response.writeHead(200, {"Content-Type": "text/css"});
+            response.end(style);
+            break;
+        case "/app.js":
+            response.writeHead(200, {"Content-Type": "text/pain"});
+            response.end(appjs);
+            break;
+        case "/novi-zadatak":
+            var ime = "";
+            var zadatak = "";
+            request.on("data", function(data) {
+                zadatak += data;
+            });
+            request.on("end", function() {
+                zadatak = JSON.parse(zadatak);
+                //console.log(zadatak);
+                ime = zadatak.ime;
+                zadatak_(zadatak);
+                var odgovor = [];
 
-				for(var i = 0; i<zadaci.length; i++) {
-					//console.log("///"+zadaci[i].ime+"///"+ime);
-					if(zadaci[i].ime == ime) {
-						odgovor.push({"zadatak" : zadaci[i].zadatak});
-					}
-				}
-				response.end(JSON.stringify(odgovor));
-			});
-			//console.log(zadaci)
-			break;
-		case "/korisnik":
-			var korisnik = "";
-			request.on("data", function(data) {
-				korisnik += data;
-			});
-			request.on("end", function() {
-				korisnik = JSON.parse(korisnik);
-				response.end(korisnik_(korisnik));
-			});
-			//console.log(korisnici);
-			break;
-		default:
-			greskaURL(response);
-			break;
-	}
+                for(var i = 0; i<zadaci.length; i++) {
+                    //console.log("///"+zadaci[i].ime+"///"+ime);
+                    if(zadaci[i].ime == ime) {
+                        odgovor.push({"zadatak" : zadaci[i].zadatak});
+                    }
+                }
+                response.end(JSON.stringify(odgovor));
+            });
+            //console.log(zadaci)
+            break;
+        case "/korisnik":
+            var korisnik = "";
+            request.on("data", function(data) {
+                korisnik += data;
+            });
+            request.on("end", function() {
+                korisnik = JSON.parse(korisnik);
+                response.end(korisnik_(korisnik));
+            });
+            //console.log(korisnici);
+            break;
+        default:
+            greskaURL(response);
+            break;
+    }
 }
 
 var server = http.createServer(odgovorServera);
